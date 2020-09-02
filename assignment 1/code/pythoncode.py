6cef4edcda0a7acc4e07b127255d71ea805969c6
pulkitsaxena92 <70573390+pulkitsaxena92@users.noreply.github.com>
2020-09-02 09:44:52 -0700

    Add files via upload

---
+++ assignment 1/code/pythoncode.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt
+x1 = [9,0]
+y1 = [0,3]
+plt.plot(x1, y1, label = "Line Equation 1")
+x2 = [5,0]
+y2 = [8,-7]
+plt.plot(x2, y2, label = "Line Equation 2")
+plt.xlabel('x - axis')
+plt.ylabel('y - axis')
+plt.title('Solution No:49')
+plt.legend()
+plt.show()
